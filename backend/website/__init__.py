370d5b2653ad58c4426c950b13cb606166c2f296
JO.O PRo <115453231+youssef-elmelegy@users.noreply.github.com>
2024-08-19 02:11:39 +0300

    Update __init__.py

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins="http://localhost:5173", supports_credentials=True)
+    CORS(app, origins="https://auth-app-front-tan.vercel.app", supports_credentials=True)
     app.config['SECRET_KEY'] = 'password'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
